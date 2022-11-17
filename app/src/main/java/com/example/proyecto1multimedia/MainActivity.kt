e6acc3bf033c85bc70bf5ae5b61faaeaddc8930c
MarBenDAM2 <marben965@alu.centroafuera.es>
2022-11-17 14:18:24 +0100

    Final

--- app/src/main/java/com/example/proyecto1multimedia/MainActivity.kt
+++ app/src/main/java/com/example/proyecto1multimedia/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.proyecto1multimedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.widget.ActivityChooserView
+import com.example.proyecto1multimedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var onCreate = 0
@@ -11,11 +14,20 @@ class MainActivity : AppCompatActivity() {
     private var onStop = 4
     private var onDestroy = 5
     private var onRestart = 6
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        //Esto hay que ponerlo tanto en ViewBinding como en ViewId
         setContentView(R.layout.activity_main)
         println("onCreate $onCreate")
+
+        /*  Metodo findviewbyid
+        *   nombre=findViewById(R.id.editTextTextPersonName)
+        *   apellido=findViewById(R.id.editTextTextPersonName2)
+        */
     }
 
     override fun onStart() {
@@ -44,5 +56,32 @@ class MainActivity : AppCompatActivity() {
     override fun onRestart() {
         super.onRestart()
         println("onRestart $onRestart")
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        binding.editTextTextPersonName.text.toString()
+        binding.editTextTextPersonName2.text.toString()
+
+        /* Metodo findviewbyid
+        *  savedInstanceState.putString("nombre", nombre.text.toString())
+        *  savedInstanceState.putString("apellido", apellido.text.toString())
+        */
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        //Metodo binding
+        binding.editTextTextPersonName.setText(savedInstanceState.getString("nombre"))
+        binding.editTextTextPersonName2.setText(savedInstanceState.getString("apellido"))
+
+        /* Metodo con findviewbyid
+        * nombre.setText(savedInstanceState.getString("nombre"))
+        * apellido.setText(savedInstanceState.getString("apellido"))
+        */
+
+
     }
 }
